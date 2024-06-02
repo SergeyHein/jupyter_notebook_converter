5f0e7f12a0f384c8623f66668ee2f44a0a8548e8
Sergey Hein <sergey.hein@gmail.com>
2024-06-02 18:10:42 +0200

    fixes

--- src/convert.sh
+++ src/convert.sh
@@ -2,8 +2,18 @@
 # $1 - ipynb file located in /data
 
 FILE="$1"
-
+FILE_WITHOUT_EXT="${FILE%.*}"
 cd /data
 jupyter nbconvert "$1" --to pdf --no-prompt
 jupyter nbconvert "$1" --to latex --no-prompt
 jupyter nbconvert "$1" --to html --no-prompt
+
+cp "${FILE_WITHOUT_EXT}.tex" "${FILE_WITHOUT_EXT}_updated.tex"
+
+
+sed -i '/\\textbf{Table of contents}/,/^$/{
+  /\\textbf{Table of contents}/a\
+\\tableofcontents\n
+  /\\textbf{Table of contents}/,/^$/d
+}' "${FILE_WITHOUT_EXT}_updated.tex"
+
